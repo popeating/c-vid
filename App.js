005ec46639280a47d71982bedb2442bdc81312a9
popeating <pop@popeating.it>
2020-10-03 22:20:22 +0200

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import url from './config/config';
+import AsyncStorage from '@react-native-community/async-storage';
+
+const storeData = async (value) => {
+  try {
+    const jsonValue = JSON.stringify(value);
+    await AsyncStorage.setItem('@latest', jsonValue);
+  } catch (e) {
+    // saving error
+  }
+};
+
+const getData = async () => {
+  try {
+    const jsonValue = await AsyncStorage.getItem('@latest');
+    return jsonValue != null ? JSON.parse(jsonValue) : null;
+  } catch (e) {
+    // error reading value
+  }
+};
 
 export default function App() {
+  const [mydata, setData] = useState([]);
+  useEffect(() => {
+    fetch(url.INCREMENTAL, {})
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        setData(result);
+      });
+  }, []);
+  const mylen = mydata.length;
+
+  if (mydata.length === 0) {
+    return (
+      <View style={styles.container}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>
+        {mydata[mylen].data} - {mydata[222].nuovi_positivi}
+        Variazione da ieri
+        {mydata[222].nuovi_positivi - mydata[221].nuovi_positivi}
+      </Text>
+
       <StatusBar style="auto" />
     </View>
   );
---
+++ config/config.js
@@ -0,0 +1,7 @@
+import react from 'react';
+export default {
+  LATEST:
+    'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale-latest.json',
+  INCREMENTAL:
+    'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale.json',
+};
