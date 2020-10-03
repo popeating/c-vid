9004f855fd6266572d6facaffb1940ef6af16476
popeating <pop@popeating.it>
2020-10-03 22:41:06 +0200

    initial skeleton

--- App.js
+++ App.js
@@ -44,12 +44,25 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>
-        {mydata[mylen].data} - {mydata[222].nuovi_positivi}
-        Variazione da ieri
-        {mydata[222].nuovi_positivi - mydata[221].nuovi_positivi}
-      </Text>
-
+      <Text>{mydata[222].data}</Text>
+      <View style={[styles.positivi]}>
+        <Text>Nuovi positivi {mydata[222].nuovi_positivi}</Text>
+        <Text>
+          Variazione da ieri{' '}
+          {mydata[222].nuovi_positivi - mydata[221].nuovi_positivi}
+        </Text>
+      </View>
+      <View style={[styles.decessi]}>
+        <Text>Descessi {mydata[222].deceduti - mydata[221].deceduti}</Text>
+        <Text>In totale {mydata[222].deceduti}</Text>
+      </View>
+      <View style={[styles.tamponi]}>
+        <Text>
+          Tamponi {mydata[222].tamponi - mydata[221].tamponi} / Testati{' '}
+          {mydata[222].casi_testati - mydata[221].casi_testati}
+        </Text>
+        <Text>In totale {mydata[222].tamponi}</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -61,5 +74,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  positivi: {
+    padding: 20,
+    backgroundColor: '#dadada',
+  },
+  decessi: {
+    padding: 20,
+    backgroundColor: '#ff0000',
+  },
+  tamponi: {
+    padding: 20,
+    backgroundColor: '#adadad',
   },
 });
