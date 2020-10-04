75de4d3a3f80cc020a84af0e5f1ee7cca886839c
popeating <pop@popeating.it>
2020-10-04 11:32:58 +0200

    Getting latest data

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import url from './config/config';
 import AsyncStorage from '@react-native-community/async-storage';
+import { format } from 'date-fns';
 
 const storeData = async (value) => {
   try {
@@ -44,22 +45,25 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>{mydata[222].data}</Text>
+      {<Text>{Date.parse(mydata[mylen - 1].data)}</Text>}
       <View style={[styles.positivi]}>
-        <Text>Nuovi positivi {mydata[222].nuovi_positivi}</Text>
+        <Text>Nuovi positivi {mydata[mylen - 1].nuovi_positivi}</Text>
         <Text>
           Variazione da ieri{' '}
-          {mydata[222].nuovi_positivi - mydata[221].nuovi_positivi}
+          {mydata[mylen - 1].nuovi_positivi - mydata[mylen - 2].nuovi_positivi}
         </Text>
       </View>
       <View style={[styles.decessi]}>
-        <Text>Descessi {mydata[222].deceduti - mydata[221].deceduti}</Text>
-        <Text>In totale {mydata[222].deceduti}</Text>
+        <Text>
+          Descessi {mydata[mylen - 1].deceduti - mydata[mylen - 2].deceduti}
+        </Text>
+        <Text>In totale {mydata[mylen - 1].deceduti}</Text>
       </View>
       <View style={[styles.tamponi]}>
         <Text>
-          Tamponi {mydata[222].tamponi - mydata[221].tamponi} / Testati{' '}
-          {mydata[222].casi_testati - mydata[221].casi_testati}
+          Tamponi {mydata[mylen - 1].tamponi - mydata[mylen - 2].tamponi} /
+          Testati{' '}
+          {mydata[mylen - 1].casi_testati - mydata[mylen - 2].casi_testati}
         </Text>
         <Text>In totale {mydata[222].tamponi}</Text>
       </View>
