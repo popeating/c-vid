63bc24970aeaad2dc056a188939bedbb584fde4e
popeating <pop@popeating.it>
2020-10-04 17:29:57 +0200

    Added more data

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import url from './config/config';
 import AsyncStorage from '@react-native-community/async-storage';
 import { format } from 'date-fns';
@@ -50,16 +50,37 @@ export default function App() {
   const date_update = format(last_update, 'dd MMMM yyyy', { locale: itloc });
   const time_update = format(last_update, 'HH:MM', { locale: itloc });
   const positivi = mydata[mylen - 1].nuovi_positivi;
+  const tot_positivi = mydata[mylen - 1].totale_positivi;
   var new_variation =
     mydata[mylen - 1].nuovi_positivi - mydata[mylen - 2].nuovi_positivi;
-  new_variation = (new_variation > 0 ? '+' : '-') + new_variation;
+  new_variation = (new_variation > 0 ? '+' : '') + new_variation;
 
   const death_variation =
     mydata[mylen - 1].deceduti - mydata[mylen - 2].deceduti;
 
   const tamponi = mydata[mylen - 1].tamponi - mydata[mylen - 2].tamponi;
+  const testati =
+    mydata[mylen - 1].casi_testati - mydata[mylen - 2].casi_testati;
 
   const rapporto = ((positivi / tamponi) * 100).toFixed(2) + '%';
+  const rapporto_t = ((positivi / testati) * 100).toFixed(2) + '%';
+
+  const guariti =
+    mydata[mylen - 1].dimessi_guariti - mydata[mylen - 2].dimessi_guariti;
+  const guariti_tot = mydata[mylen - 1].dimessi_guariti;
+  const terapia_intensiva = mydata[mylen - 1].terapia_intensiva;
+  var var_terapia_intensiva =
+    mydata[mylen - 1].terapia_intensiva - mydata[mylen - 2].terapia_intensiva;
+
+  var_terapia_intensiva =
+    (var_terapia_intensiva > 0 ? '+' : '') + var_terapia_intensiva;
+
+  const ospedalizzati = mydata[mylen - 1].totale_ospedalizzati;
+
+  var var_ospedalizzati =
+    mydata[mylen - 1].totale_ospedalizzati -
+    mydata[mylen - 2].totale_ospedalizzati;
+  var_ospedalizzati = (var_ospedalizzati > 0 ? '+' : '') + var_ospedalizzati;
 
   return (
     <View style={styles.container}>
@@ -75,24 +96,36 @@ export default function App() {
         </View>
       </View>
       <StatusBar style="auto" />
-
-      <View style={[styles.positivicontainer, styles.boxes]}>
-        <Text style={[styles.title]}>Nuovi positivi {positivi}</Text>
-        <Text>Variazione da ieri {new_variation}</Text>
-      </View>
-      <View style={[styles.decessicontainer, styles.boxes]}>
-        <Text style={[styles.title]}>Decessi {death_variation}</Text>
-        <Text>In totale {mydata[mylen - 1].deceduti}</Text>
-      </View>
-      <View style={[styles.tamponicontainer, styles.boxes]}>
-        <Text style={[styles.title]}>
-          Tamponi {tamponi} / Testati{' '}
-          {mydata[mylen - 1].casi_testati - mydata[mylen - 2].casi_testati}
-        </Text>
-        <Text>In totale {mydata[222].tamponi}</Text>
-        <Text>Rapporto positivi/tamponi {rapporto}</Text>
-      </View>
-      <StatusBar style="auto" />
+      <ScrollView style={{ width: '80%' }}>
+        <View style={[styles.positivicontainer, styles.boxes]}>
+          <Text style={[styles.title]}>Totale positivi {tot_positivi}</Text>
+          <Text style={[styles.title]}>Nuovi positivi {positivi}</Text>
+          <Text>Variazione da ieri {new_variation}</Text>
+        </View>
+        <View style={[styles.decessicontainer, styles.boxes]}>
+          <Text style={[styles.title]}>Decessi {death_variation}</Text>
+          <Text>In totale {mydata[mylen - 1].deceduti}</Text>
+        </View>
+        <View style={[styles.tamponicontainer, styles.boxes]}>
+          <Text style={[styles.title]}>
+            Tamponi {tamponi} / Testati {testati}
+          </Text>
+          <Text>In totale {mydata[222].tamponi}</Text>
+          <Text>Rapporto positivi/tamponi {rapporto}</Text>
+          <Text>Rapporto positivi/testati {rapporto_t}</Text>
+        </View>
+        <View style={[styles.hospcontainer, styles.boxes]}>
+          <Text style={[styles.title]}>
+            Guariti {guariti} / in totale {guariti_tot}
+          </Text>
+          <Text>
+            Terapia intensiva {terapia_intensiva} ({var_terapia_intensiva})
+          </Text>
+          <Text>
+            Ospedalizzati {ospedalizzati} ({var_ospedalizzati})
+          </Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -106,7 +139,7 @@ const styles = StyleSheet.create({
   },
   boxes: {
     padding: 20,
-    width: '80%',
+    width: '100%',
     borderRadius: sizes.radius,
     marginBottom: sizes.base,
   },
@@ -141,5 +174,8 @@ const styles = StyleSheet.create({
     fontSize: sizes.title,
     fontWeight: '800',
     marginBottom: 20,
+  },
+  hospcontainer: {
+    backgroundColor: colors.canary,
   },
 });
